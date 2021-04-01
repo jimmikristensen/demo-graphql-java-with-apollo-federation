93106e5b03123df99ae11d2fe46719d54d154aa7
Jimmi Kristensen <jikr@tv2.dk>
2021-04-01 13:21:22 +0200

    removed unused plugins

--- apollo_gateway/server.js
+++ apollo_gateway/server.js
@@ -14,31 +14,6 @@ const gateway = new ApolloGateway({
   __exposeQueryPlanExperimental: false,
 });
 
-const myPlugin = {
-
-  // Fires whenever a GraphQL request is received from a client.
-  requestDidStart(requestContext) {
-    console.log('Request started! Query:\n' +
-      requestContext.request.query);
-
-    return {
-
-      // Fires whenever Apollo Server will parse a GraphQL
-      // request to create its associated document AST.
-      parsingDidStart(requestContext) {
-        console.log('Parsing started!');
-      },
-
-      // Fires whenever Apollo Server will validate a
-      // request's document AST against your GraphQL schema.
-      validationDidStart(requestContext) {
-        console.log('Validation started!');
-      },
-
-    }
-  },
-};
-
 // Pass the ApolloGateway to the ApolloServer constructor
 const server = new ApolloServer({
   gateway,
